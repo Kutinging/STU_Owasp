eeb94f543658ba288d85f4be400bf86542fa982b
丁丁 <Kutinging@users.noreply.github.com>
2017-04-02 01:05:24 +0800

    Update index.php

--- Owasp_lab1/top10/top10_fixed/index.php
+++ Owasp_lab1/top10/top10_fixed/index.php
@@ -1,3 +1,9 @@
-<?php header("Content-Type:text/html; charset=utf-8");?>
-確認要前往該網頁嗎? <h3><?php print(basename($_GET['url']));?></h3>
-<p><a href="<?php print(basename($_GET['url']));?>">確定</a></p>
+<?php 
+  header("Content-Type:text/html; charset=utf-8");
+?>
+確認要前往該網頁嗎? 
+<h3><?php print(basename($_GET['url']));?></h3>
+
+<p>
+    <a href="<?php print(basename($_GET['url']));?>">確定</a>
+</p>
